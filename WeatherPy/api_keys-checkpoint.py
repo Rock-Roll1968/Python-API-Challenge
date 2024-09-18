b21373ff8dd50e397a6da0fdb87147acecceace0
Rock-Roll1968 <ernesto.v.garcia@outlook.com>
2024-09-18 00:03:58 -0500

    Update api_keys-checkpoint.py

--- WeatherPy/api_keys-checkpoint.py
+++ WeatherPy/api_keys-checkpoint.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "ac68eb747d7d71f54c8c302a89439a32"
+weather_api_key = ""
 
 # Geoapify API Key
-geoapify_key = "4d90ede934574abe8435c0a070e962cc"
+geoapify_key = ""
